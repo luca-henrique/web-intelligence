1823a6f96455a9ae7cd5b746036213bee076e28c
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2025-04-08 23:11:57 -0300

    fix header

--- components/organisms/header.tsx
+++ components/organisms/header.tsx
@@ -55,7 +55,6 @@ export function Header() {
               <span className="absolute -bottom-1 left-0 h-0.5 w-0 bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
             </Link>
           ))}
-          {t("navigation.home")}
 
         </nav>
         <div className="flex items-center gap-2">
